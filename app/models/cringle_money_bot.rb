a9dc300b822ffbb0a33a665703a29e0199a89575
swiknaba <ludwig.reinmiedl@gmail.com>
2018-05-22 13:32:18 +0200

    Bugfixing

--- app/models/cringle_money_bot.rb
+++ app/models/cringle_money_bot.rb
@@ -4,7 +4,7 @@ require 'cringle_money'
 class CringleMoneyBot < SlackRubyBot::Bot
   help do
     title 'CringleMoney Bot'
-    desc 'Converts Money and researches currency exchange rates.'
+    desc 'Converts Money and researches currency exchange rates. All amounts need to be given without separator and are accounted as the currencies smallest unit (e.g. cents for EUR)'
 
     command '<currency_1> to <currency_2, currency_3, ..>' do
       desc 'Gives you exchange rates from one currency into multiple others (comma separated).'
@@ -27,19 +27,20 @@ class CringleMoneyBot < SlackRubyBot::Bot
 
     private
 
-    def parse_input(match)
-      from, to = match.split('to').strip
+    def parse_input(input)
+      from, to = input.split('to').map{ |x| x.strip }
       amount, currency = from.scan(/\d+|\D+/)
       to = to.gsub(/[,\s]/, '').scan(/.{3}/)
 
       # get exchange rates: ["USD: 1.182452", "AUD: 1.555988"]
       if currency.nil?
         result = CringleMoney.get_rates(Money::Currency.wrap(amount), to)
-        result.map { |c, a| [c, a].join(': ') }
+        result = result.map { |c, a| [c, a].join(': ') }
       # exchange money: ["23.54 USD", "31.28 AUD"]
       else
-        result = CringleMoney.new(amount, currency).exchange_to(to)
-        result.map{ |m| [(m.fractional.to_f / m.currency.subunit_to_unit), m.currency.iso_code].join(' ') }
+        result = CringleMoney.new(amount, currency.strip.upcase).exchange_to(to)
+        # result = result.map{ |m| [(m.fractional.to_f / m.currency.subunit_to_unit), m.currency.iso_code].join(' ') }
+        result = result.map { |m| [m.fractional, m.currency.iso_code].join(' ') }
       end
       result.join(' | ')
     end
